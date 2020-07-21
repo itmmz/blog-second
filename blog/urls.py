04c6f70925509439ceebee4b506e9600729ca7dc
itomizu <m.i.19971124@gmail.com>
2020-07-21 18:05:35 +0900

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('calculator', views.index, name='index'),
+    path('', views.index, name='index'),
     path('calc', views.calc),
 
 ]
