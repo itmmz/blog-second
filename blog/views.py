7a5833c210210294d99fad7ba19ee07d1d863e28
itomizu <m.i.19971124@gmail.com>
2020-07-21 20:17:29 +0900

    blog

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -34,16 +34,17 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+
+
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def index(request):
+def calculator(request):
 
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/calculator.html')
 
 
 def calc(request):
@@ -60,4 +61,20 @@ def calc(request):
 
     }
 
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/calculator.html', context)
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
